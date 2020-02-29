42b8adabd5cc10547d1ce03cc37a4d7f67b75449
Evans Shango <lutatwaevans@gmail.com>
2020-02-29 20:41:58 +0300

    allow a logged in user to delete his/her screaam

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -149,3 +149,22 @@ exports.unlikeScream = (req, res) => {
         res.status(500).json({error: err.code})
     })
 };
+
+exports.deleteScream = (req, res) => {
+    const document = database.doc(`/screams/${req.params.screamId}`);
+    document.get().then(doc => {
+        if (!doc.exists){
+            return res.status(404).json({error: 'Scream not found'})
+        }
+        if (doc.data().handle !== req.user.handle){
+            return res.status(403).json({error: 'Unauthorized request'})
+        } else {
+            return document.delete();
+        }
+    }).then(() => {
+        res.json({message: 'Scream deleted successfully'})
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({error: err.code})
+    })
+};
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const fbAuth = require('./util/fbAuth');
-const {getAllScreams, creatScream, getScream, commentOnScream, likeScream, unlikeScream} = require('./handlers/screams');
+const {
+    getAllScreams, creatScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream
+} = require('./handlers/screams');
 const {registerUser, signin, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 //scream routes
@@ -11,6 +13,7 @@ app.get('/scream/:screamId', getScream);
 app.post('/scream/:screamId/comment', fbAuth, commentOnScream);
 app.get('/scream/:screamId/like', fbAuth, likeScream);
 app.get('/scream/:screamId/unlike', fbAuth, unlikeScream);
+app.delete('/scream/:screamId', fbAuth, deleteScream);
 
 //auth routes
 app.post('/register', registerUser);
