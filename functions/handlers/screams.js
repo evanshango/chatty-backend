5c72995be2d31b4332c36123d0a5d897e01fe764
Evans Shango <lutatwaevans@gmail.com>
2020-03-05 00:21:43 +0300

    check if scream body is empty before submitting to server

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -20,6 +20,9 @@ exports.getAllScreams = (req, res) => {
 };
 
 exports.creatScream = (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty' });
+    }
     const newScream = {
         body: req.body.body,
         handle: req.user.handle,
